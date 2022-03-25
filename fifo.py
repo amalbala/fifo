acc2fb572df301b1eae616b9027b8af97d0b4f46
amalbala <amalbala@gmail.com>
2022-03-25 20:30:43 +0100

    fifo: add docstring

--- fifo.py
+++ fifo.py
@@ -1,5 +1,15 @@
 class Fifo:
+    """Class for code interview FIFO with fixed lenght known in advance, focus on speed
+
+    All operation are O(1)
+    """
+
     def __init__(self, size):
+        """Constructor of the container to reserve memory upfront
+
+        Args:
+            size (int): Size of the vector, to simulate a circular vector
+        """
         self.__size = size
         self.__push_pos = 0
         self.__pop_pos = 0
@@ -7,12 +17,22 @@ class Fifo:
         self.__container = [None] * size
 
     def push(self, element):
+        """Push an element at the end of the container
+
+        Args:
+            element (Any): Element to introduce if the container is not full
+        """
         if self.__elements < self.__size:
             self.__container[self.__push_pos] = element
             self.__push_pos = (self.__push_pos + 1) % self.__size
             self.__elements += 1
 
     def pop(self):
+        """Pop an element from the container
+
+        Returns:
+            Any: Element to remove or None if the container is empty
+        """
         if self.isEmpty():
             return None
 
@@ -22,4 +42,9 @@ class Fifo:
         return self.__container[current_pop_pos]
 
     def isEmpty(self):
+        """Test empty
+
+        Returns:
+            bool: If the container is empty or not.
+        """
         return self.__elements == 0
